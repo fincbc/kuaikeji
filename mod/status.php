293f1b308bd2677fa7b9d0c1de73cdf541e42a04
fincbc <finc44@qq.com>
2016-07-03 16:13:33 +0800

    first commit

---
+++ mod/status.php
@@ -0,0 +1,67 @@
+<div id="status">
+	<div class="center">
+		<div class="location">
+			<span>送至: <i id="addr">上海</i></span>
+			<ul class="location-tab">
+				<li>北京</li>
+				<li>上海</li>
+				<li>天津</li>
+				<li>重庆</li>
+				<li>河北</li>
+				<li>山西</li>
+				<li>河南</li>
+				<li>辽宁</li>
+				<li>吉林</li>
+				<li>黑龙江</li>
+			</ul>
+		</div>
+		<ul class="status-ul">
+			<li>
+				<div>
+					<a class="hr" href="">网站导航</a>
+				</div>
+			</li>
+			<li>
+				<div>
+					<a class="hr" href="/">客户服务</a>
+				</div>
+			</li>
+			<li>
+				<div>
+					<a class="hr" href="/">关注京东</a>
+				</div>
+			</li>
+			<li>
+				<div>
+					<a class="hr" href="/">手机京东</a>
+				</div>
+			</li>
+			<li>
+				<div>
+					<a class="hr" href="/">企业采购</a>
+				</div>
+			</li>
+			<li>
+				<div>
+					<a class="hr" href="/">京东会员</a>
+				</div>
+			</li>
+			<li>
+				<div>
+					<a class="hr" href="/">我的京东</a>
+				</div>
+			</li>
+			<li>
+				<div>
+					<a class="hr" href="/">我的订单</a>
+				</div>
+			</li>
+			<li>
+				<div>
+					<a class="hr" href="/">你好,请登录</a>
+					<a class="r" href="/">免费注册</a>
+				</div>
+			</li>
+		</ul>
+	</div>
+</div>
---
+++ old/mofang/test.php
@@ -0,0 +1,122 @@
+<html>
+<head>
+	<title><?php echo date("Y-m-d") ?></title>
+    <meta http-equlv="content-type" conmten="text/html;charset=utf8">
+    <link rel="stylesheet" type="text/css" href="css/layout.css">
+			
+
+</head>
+<body>
+	 <div id="container">
+	 	<div id="a1">
+	 		<div id="logo">
+	 			
+	 		</div>
+	 		<div id="bamner">
+	 			
+	 		</div>
+	 		<div id="tool">
+	 			
+	 		</div>
+	 		<div class="fenge">
+	 			
+	 		</div>
+	 		<div id="menu">
+	 			<ul>
+	 				<?php
+	 					//查询数据库
+	 					for($i=0; $i < count($list); $i++) {
+	 						echo "<li><a href='#'>四川旅游</a></li>";
+	 					}
+	 				?>
+	 				<li>
+	 					<a href="#">四川旅游</a>
+	 				</li>
+	 				<div class="tiao"></div>
+
+	 			</ul>
+	 		</div>
+
+
+	 	</div >
+	 	<div class="fenge"></div>
+	 	<div id="a2">
+	 		<div class="leftbox">
+	 			<div class="left">
+	 				<div class="tit">
+	 					<h3>四川线路</h3>
+	 				</div>
+	 				<div class="content">
+	 					<ul>
+	 						<li><a href="$">ttttttt</a></li>
+	 						<li><a href="$">ttttttt</a></li>
+	 						<li><a href="$">ttttttt</a></li>
+	 						<li><a href="$">ttttttt</a></li>
+
+	 					</ul>
+	 				</div>
+	 			</div>
+	 			<div class="right">
+	 				<div class="tit">
+	 					<h3>云南线路</h3>
+	 				</div>
+	 				<div class="content2"></div>
+	 			</div>
+	 			<div class="fenge">
+	 				
+	 			</div>
+	 			<div class="left">
+	 				<div class="tit">
+	 					<h3>泰国线路</h3>
+	 				</div>
+	 				<div class="content"></div>
+	 			</div>
+	 			<div class="right">
+	 				<div class="tit">
+	 					<h3>欧洲线路</h3>
+	 				</div>
+	 				<div class="content2"></div>
+	 			</div>
+	 		</div>
+	 		<div class="rightbox"></div>
+
+
+	 	</div>
+		<div class="fenge">
+		
+		</div>
+		<div id="gg"></div>
+		<div class="fenge">
+		
+		</div>
+	 	<div id="a2">
+	 		<div class="leftbox">
+	 			<div class="left">
+	 				
+	 			</div>
+	 			<div class="right">
+	 				
+	 			</div>
+	 			<div class="fenge">
+	 				
+	 			</div>
+	 			<div class="left">
+	 				
+	 			</div>
+	 			<div class="right">
+	 				
+	 			</div>
+	 		</div>
+	 		<div class="rightbox"></div>
+
+
+	 	</div>
+	 	<div class="fenge"></div>
+	 	<div id="a3">
+
+
+
+	 	</div>
+	 </div>
+</body>
+</html>
